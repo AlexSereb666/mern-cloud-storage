97edcda414ddd08921a9ebd947071df9cdeeef3a
Alexey <Alexey@mail.ru>
2023-06-17 22:30:41 +0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const config = require("config")
+const fileUpload = require("express-fileupload")
+const authRouter = require("./routers/auth.router")
+const fileRouter = require("./routers/file.router");
+const corsMiddleware = require('./middleware/cors.middleware');
+
+const app = express()
+const PORT = config.get('serverPort')
+
+app.use(fileUpload({}))
+app.use(corsMiddleware)
+app.use(express.json())
+app.use("/api/auth", authRouter)
+app.use("/api/files", fileRouter)
+
+
+const start = async () => {
+    try {
+        await mongoose.connect(config.get("dbUrl"), {
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        })
+
+        app.listen(PORT, () => {
+            console.log('Server started on port ', PORT)
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
